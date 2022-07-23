3f947ccdf1860f769982d4404b131976ae448a60
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-07-23 01:17:17 -0800

    Create anagram.js

---
+++ anagram.js
@@ -0,0 +1,12 @@
+var firstWord = "Mary";
+var secondWord = "Army";
+isAnagram(firstWord, secondWord); // true
+function isAnagram(first, second) {
+// For case insensitivity, change both words to lowercase.
+var a = first.toLowerCase();
+var b = second.toLowerCase();
+// Sort the strings, and join the resulting array to a string. Compare the results
+a = a.split("").sort().join("");
+b = b.split("").sort().join("");
+return a === b;
+}
